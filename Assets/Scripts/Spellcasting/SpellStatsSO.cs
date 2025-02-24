f8ca3015d2816f67dbfdd6d7d3fb184813569add
ortjk <ortjkdev@gmail.com>
2025-02-24 10:13:47 -0330

    Added spell stat struct and list

--- Assets/Scripts/Spellcasting/SpellStatsSO.cs
+++ Assets/Scripts/Spellcasting/SpellStatsSO.cs
@@ -1,7 +1,20 @@
+using System;
 using UnityEngine;
+
+[Serializable]
+public struct SpellStat
+{
+    public string spellName;
+    public float damage;
+    public float cooldown;
+    public float mana;
+    public float speed;
+    public float range;
+    public float area;
+}
 
 [CreateAssetMenu(fileName = "SpellStatsSO", menuName = "Scriptable Objects/SpellStatsSO")]
 public class SpellStatsSO : ScriptableObject
 {
-    
+    public SpellStat[] spellStats;
 }
