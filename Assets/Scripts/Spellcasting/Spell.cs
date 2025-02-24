e28c34a49ea7ea4f09c2e857cf238223ec11870c
ortjk <ortjkdev@gmail.com>
2025-02-24 09:37:35 -0330

    Added base spell class outline

---
+++ Assets/Scripts/Spellcasting/Spell.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public abstract class Spell: MonoBehaviour
+{
+    [Header("Spell Data")]
+    [SerializeField] protected SpellStatsSO _spell_stats;
+    [SerializeField] protected int _spell_id;
+
+    public abstract void cast(Vector3 direction);
+}
---
+++ Assets/Scripts/Spellcasting/SpellStatsSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpellStatsSO", menuName = "Scriptable Objects/SpellStatsSO")]
+public class SpellStatsSO : ScriptableObject
+{
+    
+}
